d3dd3b64fd3fafc5270bbfc2f94fc196d1b2c375
Si0n <sionix1@ya.ru>
2015-05-04 11:03:29 +0300

    04.05.15

--- lib/search_action.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'classLoader.php';
-$page = new Ini();
-$page->loadLibs();
-$db = new StudentsMapper($pdo);
-$search = isset($_GET['search'])   ? makeProtect( $_GET['search'] ) : '' ;
-    $data = ($db->searchFromStudents($search));
-include 'paginator.php';
-
