fe03e285ef3bf78c4cd64d7ef4e199f588fbc254
Si0n <sionix1@ya.ru>
2015-06-05 13:24:35 +0300

    05/06/15

--- lib/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function makeProtect($in)
-{
-    return htmlspecialchars($in, ENT_QUOTES);
-}
-function getCookie($name)
-{
-    return makeProtect($_COOKIE[$name]);
-}
--- template/list2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$numpages = 5;
-for($i=1;$i<=$numpages;$i++):?>
-    <a href="list2.php?num=<?= $i?> "><?= $i?></a>
-<?php endfor; ?>
